5fb27ea67dcfb4de73849c36513a24642bbec0e7
Tsung Yu <sammy849754@gmail.com>
2025-08-30 23:03:13 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/vue-emit-props/',// 將 <repo-name> 換成你的 repo 名稱
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
